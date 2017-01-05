f52b9698cd1cd27a597936eb8d02c50439ea0dc6
Anna Dolan <anna.dolan@gmail.com>
2017-01-05 11:19:16 -0700

    Add model tests for UserEvent

--- spec/models/user_event_spec.rb
+++ spec/models/user_event_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe UserEvent, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "relationships" do
+      it "belongs to a user" do
+        user_event = create(:user_event)
+        expect(user_event).to respond_to(:user)
+      end
+
+      it "belongs to an event" do
+        user_event = create(:user_event)
+        expect(user_event).to respond_to(:event)
+      end
+
+      it "belongs to a role" do
+        user_event = create(:user_event)
+        expect(user_event).to respond_to(:role)
+      end
+    end
+  end
 end
