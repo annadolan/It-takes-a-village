438c411a9bc7537a1110c1b63e8fd559e758c678
Anna Dolan <anna.dolan@gmail.com>
2017-01-05 14:39:07 -0700

    Remove unnecessary validations from user along with corresponding tests

--- spec/features/guests/guest_can_create_an_account_spec.rb
+++ spec/features/guests/guest_can_create_an_account_spec.rb
@@ -30,24 +30,14 @@ describe "Visit new uer path" do
     end
 
     scenario "Must enter required info or is shown sign up page again" do
-
-      # cat_1, cat_2 = create_list(:category, 2)
       user = build(:user)
 
       visit new_user_path
 
       fill_in "user[first_name]", with: user.first_name
-      fill_in "user[last_name]", with: user.last_name
-      fill_in "user[street_number]", with: user.street_number
-      fill_in "user[unit_number]", with: "45"
-      fill_in "user[city]", with: user.city
-      fill_in "user[state]", with: user.state
       fill_in "user[email]", with: user.email
-      fill_in "user[phone]", with: user.phone
       fill_in "user[password]", with: user.password
       fill_in "user[password_confirmation]", with: user.password
-      fill_in "user[picture]", with: user.picture
-      # select("#{cat_1.name}", from: 'user[category_id]')
 
       click_on "Create Account"
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,13 +4,7 @@ RSpec.describe User, type: :model do
   context "Validations" do
     it { should validate_presence_of(:first_name)}
     it { should validate_presence_of(:last_name)}
-    it { should validate_presence_of(:street_number)}
-    it { should validate_presence_of(:city)}
-    it { should validate_presence_of(:state)}
-    it { should validate_presence_of(:zip_code)}
     it { should validate_presence_of(:email)}
-    it { should validate_presence_of(:picture)}
-    it { should validate_presence_of(:phone)}
     it { should validate_presence_of(:password)}
     # it { should validate_presence_of(:category_id)}
     it { should validate_confirmation_of(:password)}
