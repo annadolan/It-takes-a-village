5eddcd8f97b9c715b99396d5631b92dc8ef82ae0
Megan Talbot <meganftalbot@gmail.com>
2017-01-04 19:36:51 -0700

    Add sad path test for user entering invaid email address

--- spec/features/guests/guest_can_create_an_account_spec.rb
+++ spec/features/guests/guest_can_create_an_account_spec.rb
@@ -53,5 +53,27 @@ describe "Visit new uer path" do
 
       expect(page).to have_content("Create a new account")
     end
+
+    scenario "Cannot enter invalid email address" do
+      user = build(:user)
+
+      visit new_user_path
+
+      fill_in "user[first_name]", with: user.first_name
+      fill_in "user[last_name]", with: user.last_name
+      fill_in "user[street_number]", with: user.street_number
+      fill_in "user[unit_number]", with: "45"
+      fill_in "user[city]", with: user.city
+      fill_in "user[state]", with: user.state
+      fill_in "user[email]", with: "bademail"
+      fill_in "user[phone]", with: user.phone
+      fill_in "user[password]", with: user.password
+      fill_in "user[password_confirmation]", with: user.password
+      fill_in "user[picture]", with: user.picture
+
+      click_on "Create Account"
+
+      expect(page).to have_content("Create a new account")
+    end
   end
 end
