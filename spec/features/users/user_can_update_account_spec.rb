3fc31dae2e8c3185a7624932e7077ef2531d8338
Anna Dolan <anna.dolan@gmail.com>
2017-01-06 16:33:47 -0700

    Remove password fields from update form

--- spec/features/users/user_can_update_account_spec.rb
+++ spec/features/users/user_can_update_account_spec.rb
@@ -11,26 +11,9 @@ feature "As a logged in user" do
     expect(current_path).to eq(edit_user_path(user))
 
     fill_in "user[email]", with: "newemail@email.com"
-    # fill_in "user[password]", with: "test"
-    # fill_in "user[password_confirmation]", with: "test"
     click_button "Update Account"
 
     expect(current_path).to eq(dashboard_path)
     expect(page).to have_content("#{user.first_name}'s account updated!")
   end
-
-  # scenario "I cannot edit my account information without my password" do
-  #   user = create(:user)
-  #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-  #   visit user_path(user)
-  #
-  #   click_on "Edit your account"
-  #
-  #   expect(current_path).to eq(edit_user_path(user))
-  #
-  #   fill_in "user[email]", with: "newemail@email.com"
-  #   click_button "Update Account"
-  #
-  #   expect(page).to have_content("Account didn't update. Try again.")
-  # end
 end
