7baf646b551822c450f6c01f1e0c7658bf2190f4
Anna Dolan <anna.dolan@gmail.com>
2017-01-07 18:23:03 -0700

    Remove picture column from user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170108003926) do
+ActiveRecord::Schema.define(version: 20170108012203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,7 +66,6 @@ ActiveRecord::Schema.define(version: 20170108003926) do
     t.string   "state"
     t.string   "zip_code"
     t.string   "email"
-    t.string   "picture"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
     t.string   "unit_number"
