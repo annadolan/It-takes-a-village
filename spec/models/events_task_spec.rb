8cc6b34f98e42a79aa7e7c654ddc6779dd0ced39
Anna Dolan <anna.dolan@gmail.com>
2017-01-11 11:18:57 -0700

    Add relationship tests for events task

--- spec/models/events_task_spec.rb
+++ spec/models/events_task_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe EventsTask, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "relationships" do
+      it "belongs to an event" do
+        events_task = create(:events_task)
+        expect(events_task).to respond_to(:event)
+      end
+
+      it "belongs to a task" do
+        events_task = create(:events_task)
+        expect(events_task).to respond_to(:task)
+      end
+    end
+  end
 end
