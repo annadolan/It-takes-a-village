7fb5cfb4a5671f6461f6f1ac8e5a527cb9711bf3
Anna Dolan <anna.dolan@gmail.com>
2017-01-10 21:16:45 -0700

    Style new confirmation page

---
+++ app/assets/javascripts/events.js
@@ -0,0 +1,9 @@
+// var getCategoriesForEvent = function(){
+//   var eventId = $('input[name="show-id"]').val();
+//   return $.ajax({
+//     method: 'GET',
+//     url: API + '/api/v1/events/' + eventId
+//   })
+//   .done(onGetSuccess)
+//   .fail(onFail);
+// }
