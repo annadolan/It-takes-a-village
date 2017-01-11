bab1c4df4b9c190cde8d8f4aeb3d6eff06d85e72
Anna Dolan <anna.dolan@gmail.com>
2017-01-11 14:11:12 -0700

    Update feature test for event creation

--- spec/features/users/user_can_create_event_as_new_parent_spec.rb
+++ spec/features/users/user_can_create_event_as_new_parent_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 describe "As a logged in user" do
-  scenario "I can create an event with categories and choose my role" do
+  scenario "I can create an event with categories and choose my role and tasks on each category" do
     user = create(:user)
     role = create(:role, name: "Organizer")
     role1 = create(:role, name: "New Parent")
     categories = create_list(:category, 3)
+    task1 = create(:task, category_id: categories.first.id, name: "A")
+    task2 = create(:task, category_id: categories.first.id, name: "B")
+    task3 = create(:task, category_id: categories.last.id, name: "C")
+    task4 = create(:task, category_id: categories.last.id, name: "D")
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -26,5 +30,19 @@ describe "As a logged in user" do
     click_on "Create Event"
 
     expect(current_path).to eq(edit_event_path(Event.last))
+
+    within("#category-#{categories.first.id}") do
+      expect(page.find('.multiselect')[:multiple]).to be_truthy
+    end
+
+    within("#category-#{categories.last.id}") do
+      expect(page.find('.multiselect')[:multiple]).to be_truthy
+    end
+
+
+    expect(page).to have_button("Add selected tasks")
+
+
+
   end
 end
