c603d55f5353391563af9d32b882faa6dd82380f
Megan Talbot <meganftalbot@gmail.com>
2017-01-11 08:58:13 -0700

    Add small events tasks update

--- app/controllers/events_tasks_controller.rb
+++ app/controllers/events_tasks_controller.rb
@@ -9,6 +9,7 @@ class EventsTasksController < ApplicationController
     @event = Event.find(params["events_task"]["event_id"])
     @event_task = EventsTask.find(params["events_task"]["event_task_id"])
     @event_task.update(start_time: params["events_task"]["start_time"])
+    
     redirect_to :back
   end
 
