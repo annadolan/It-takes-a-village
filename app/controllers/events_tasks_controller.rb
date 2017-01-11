e72f508402a97ff559f5f998dcdf32a39d335112
Megan Talbot <meganftalbot@gmail.com>
2017-01-11 14:23:26 -0700

    Alter calendar view to be a table

--- app/controllers/events_tasks_controller.rb
+++ app/controllers/events_tasks_controller.rb
@@ -12,12 +12,12 @@ class EventsTasksController < ApplicationController
       @event_task.update_attributes(date: params['events_tasks'][id]["date"])
     end
     flash[:success] = "Time updated!"
-    redirect_to :back
+    redirect_to events_tasks_path
   end
 
   def show
     @event = Event.find(params[:id])
-    @meetings = @event.events_tasks
+    @events_tasks = @event.events_tasks
 
     # @tasks = {}
     # @event_tasks.each do |task|
