84e4b0ce44d75924e0736fd3106915ff6de1b939
Anna Dolan <anna.dolan@gmail.com>
2017-01-11 12:41:18 -0700

    Event creation feature test

--- spec/features/users/user_can_create_event_as_new_parent_spec.rb
+++ spec/features/users/user_can_create_event_as_new_parent_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
-xdescribe "As a logged in user" do
-  scenario "I can create an event as a new parent" do
+describe "As a logged in user" do
+  scenario "I can create an event with categories and choose my role" do
     user = create(:user)
-    role = create(:role, name: "New Parent")
+    role = create(:role, name: "Organizer")
+    role1 = create(:role, name: "New Parent")
+    categories = create_list(:category, 3)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -13,6 +15,16 @@ xdescribe "As a logged in user" do
 
     expect(current_path). to eq(new_event_path)
 
+    fill_in "event-name", with: "My event"
 
+    choose("event_user_event_role_id_#{role.id}")
+
+
+    check("event_category_ids_#{categories.first.id}")
+    check("event_category_ids_#{categories.last.id}")
+
+    click_on "Create Event"
+
+    expect(current_path).to eq(edit_event_path(Event.last))
   end
 end
