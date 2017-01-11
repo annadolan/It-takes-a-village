41664d6834b1296955f78687872860f4695abb35
Anna Dolan <anna.dolan@gmail.com>
2017-01-11 08:55:33 -0700

    Remove unused js file

--- app/assets/javascripts/events.js
+++
@@ -1,9 +0,0 @@
-// var getCategoriesForEvent = function(){
-//   var eventId = $('input[name="show-id"]').val();
-//   return $.ajax({
-//     method: 'GET',
-//     url: API + '/api/v1/events/' + eventId
-//   })
-//   .done(onGetSuccess)
-//   .fail(onFail);
-// }
